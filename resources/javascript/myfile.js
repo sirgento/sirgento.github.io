27f73a7db03ddc82d43c37e79652b2c54df3743c
sirgento <gabripr@gmail.com>
2022-05-04 21:40:50 -0300

    changed css, changed from iframe to fetch

--- resources/javascript/myfile.js
+++ resources/javascript/myfile.js
@@ -2,3 +2,8 @@ function redirect_iframe(btn_id) {
 	let redirect_url = "./pages/" + btn_id + ".html";
 	$("#content-frame")[0].src = redirect_url;
 }
+async function updateContentDiv(btn_id) {
+	let pResult = await fetch("./pages/" + btn_id + ".html");
+	let htmlText = await pResult.text();
+	$("#content-div")[0].innerHTML = htmlText;
+}
