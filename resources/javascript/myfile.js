3d0d24f9b17043f0398443a4579fed6fbefa768e
sirgento <gabripr@gmail.com>
2022-05-11 19:35:21 -0300

    added overlay scrollbars

--- resources/javascript/myfile.js
+++ resources/javascript/myfile.js
@@ -2,6 +2,17 @@ function redirect_iframe(btn_id) {
 	let redirect_url = "./pages/" + btn_id + ".html";
 	$("#content-frame")[0].src = redirect_url;
 }
+function setMaxContentHeight() {
+	let currentSum = 16; //padding
+	let vwh = visualViewport.height;
+	for (x of $("body").children()) {
+		if (x.tagName.toLowerCase() == "div" && !x.classList.contains("glass")) {
+			currentSum += x.clientHeight;
+			currentSum += 16; //padding
+		}
+	}
+	$('body').find('.glass').get(0).style.maxHeight = (vwh - currentSum) + "px";
+}
 async function updateContentDiv(btn_id) {
 	let pResult = await fetch("./pages/" + btn_id + ".html");
 	let htmlText = await pResult.text();
