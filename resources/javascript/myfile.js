bd491b72f8047ead6387dd40714e30f374183374
Gabriel Pereira <glmpereira@msn.com>
2022-08-16 18:20:19 -0300

    scaffold cardapio

--- resources/javascript/myfile.js
+++ resources/javascript/myfile.js
@@ -18,3 +18,22 @@ async function updateContentDiv(btn_id) {
 	let htmlText = await pResult.text();
 	$("#content-div")[0].innerHTML = htmlText;
 }
+
+async function fetch_json_cardapio(resourcepath) {
+	
+    let url = window.location.href + resourcepath;
+    let list = await fetch(url);
+    let items = await list.text();
+
+    let objets = JSON.parse(items);
+
+	return objets;
+}
+
+function jogar_cardapio_tela(componente, cardapio) {
+	for (x of cardapio) {
+		let prev_data = componente.innerHTML;
+		let new_data = `<div><div><span>${x.name}</span><span>${x.price}</span></div><div><span>${x.desc}</span></div></div>`;
+		componente.innerHTML = prev_data + new_data;
+	}
+}
